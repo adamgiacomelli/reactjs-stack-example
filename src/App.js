9c48576b44c5b28da9891fd6d87629f3e845ec45
Adam Giacomelli <adam.giacom@gmail.com>
2018-08-27 11:19:58 +0200

    Example styling with materialUI

--- src/App.js
+++ src/App.js
@@ -1,13 +1,29 @@
 import React, { Component } from 'react';
+import { withStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
 import SpecificExampleComponent from './components/specific-example-component';
 
+const styles = {
+  root: {
+    flexGrow: 1,
+  },
+};
+
+@withStyles(styles)
 class App extends Component {
   render() {
+    const { classes } = this.props;
     return (
-      <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">Welcome to React Stack Example</h1>
-        </header>
+      <div className={classes.root}>
+        <AppBar position="static" color="default">
+          <Toolbar>
+            <Typography variant="title" color="inherit">
+            Welcome to React Stack Example
+            </Typography>
+          </Toolbar>
+        </AppBar>
         <SpecificExampleComponent />
       </div>
     );
--- src/components/specific-example-component/index.js
+++ src/components/specific-example-component/index.js
@@ -1,8 +1,30 @@
 import React, { Component } from 'react';
 import { inject, observer } from 'mobx-react';
-import { ExampleButton } from '../common';
+
+import { withStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
 import crow from '../../assets/crow.jpg';
 
+const styles = {
+  card: {
+    maxWidth: 500,
+    margin: 10,
+  },
+  media: {
+    height: 240,
+  },
+  code: {
+    maxWidth: 452,
+    overflow: 'auto',
+    maxHeight: 250,
+  },
+};
+@withStyles(styles)
 @inject('rootStore')
 @observer
 class SpecificExampleComponent extends Component {
@@ -20,24 +42,42 @@ class SpecificExampleComponent extends Component {
       rootStore: {
         exampleStore: { data, loading },
       },
+      classes,
     } = this.props;
 
     return (
-      <div>
-        <img alt="crow" src={crow} />
-        <p>
-          To get started, edit
-          {' '}
-          <code>src/App.js</code>
-          {' '}
+      <React.Fragment>
+        <Card className={classes.card}>
+          <CardMedia className={classes.media} image={crow} title="Crow" />
+          <CardContent>
+            <Typography gutterBottom variant="headline" component="h2">
+              Crow
+            </Typography>
+            <Typography component="p">
+              To get started, edit
+              {' '}
+              <code>src/App.js</code>
+              {' '}
 and save to reload.
-        </p>
-        <ExampleButton onClick={this.onClick} label="Click me to test API" />
-        <pre>
-          {!loading && data && data.map((prop, idx) => <p key={idx}>{prop}</p>)}
-          {loading && 'loading...'}
-        </pre>
-      </div>
+            </Typography>
+          </CardContent>
+          <CardActions>
+            <Button color="primary" onClick={this.onClick}>
+              Click me to test API
+            </Button>
+          </CardActions>
+        </Card>
+        <Card className={classes.card}>
+          <CardContent>
+            <pre className={classes.code}>
+              {!loading
+                && data
+                && data.map((prop, idx) => <p key={idx}>{prop}</p>)}
+              {loading && 'loading...'}
+            </pre>
+          </CardContent>
+        </Card>
+      </React.Fragment>
     );
   }
 }
