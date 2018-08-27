6fd553e6521f553dc1d51251ede15266222995e0
Adam Giacomelli <adam.giacom@gmail.com>
2018-08-27 10:34:28 +0200

    Added Fela & configured fela renderer

--- src/index.js
+++ src/index.js
@@ -1,16 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'mobx-react';
-import App from './App';
+import { Provider as MobxProvider } from 'mobx-react';
 
+import { createRenderer } from 'fela';
+import { Provider as FelaProvider } from 'react-fela';
 import { RootStore } from './stores';
+
+import App from './App';
 
 // Initialize the root store
 const rootStore = RootStore.create({});
+// Initialize the fela renderer
+const renderer = createRenderer();
 
 ReactDOM.render(
-  <Provider rootStore={rootStore}>
-    <App />
-  </Provider>,
+  <MobxProvider rootStore={rootStore}>
+    <FelaProvider renderer={renderer}>
+      <App />
+    </FelaProvider>
+  </MobxProvider>,
   document.getElementById('root'),
 );
